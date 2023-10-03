72b4f4d43e284c2ce5f5711746fd655782610cfc
Mattlewis2014@gmail.com <mattlewis2014@gmail.com>
2023-10-03 18:59:48 +0200

    started bottles py and made foo

--- bottles.py
+++ bottles.py
@@ -1,5 +1,8 @@
-def bottle_song():
-	# write your code here!
-	pass
+def bottle_song(num):
+	for i in range(num,0,-1):
+		if i >1:
+			print(f"{i} bottles of beer on the wall, {i} bottles of beer. Take one down and pass it around, {i-1} bottles of beer on the wall.")
+		elif i=1
+			print("1 bottle of beer on the wall, 1 bottle of beer. Take one down and pass it around, no more bottles of beer on the wall. No more bottles of beer on the wall, no more bottles of beer.")
 
 bottle_song()

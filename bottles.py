03600896b8a6afa16446e766ba6528491cc9b257
Mattlewis2014@gmail.com <mattlewis2014@gmail.com>
2023-10-05 18:24:42 +0200

    99 bottles roughly done

--- bottles.py
+++ bottles.py
@@ -1,8 +1,10 @@
 def bottle_song(num):
-	for i in range(num,0,-1):
+	for i in range(num,-1,-1):
 		if i >1:
 			print(f"{i} bottles of beer on the wall, {i} bottles of beer. Take one down and pass it around, {i-1} bottles of beer on the wall.")
-		elif i=1
-			print("1 bottle of beer on the wall, 1 bottle of beer. Take one down and pass it around, no more bottles of beer on the wall. No more bottles of beer on the wall, no more bottles of beer.")
+		elif i==1:
+			print("1 bottle of beer on the wall, 1 bottle of beer. Take one down and pass it around, no more bottles of beer on the wall.")
+		else:
+			print("No more bottles of beer on the wall, no more bottles of beer.No more bottles of beer on the wall, no more bottles of beer. Go to the store and buy some more, 99 bottles of beer on the wall.")
 
-bottle_song()
+bottle_song(66)
